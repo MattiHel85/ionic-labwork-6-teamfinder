a1e642bf197eea52bd4855c81182d97e4d418294
Md-Harun-Or-Rashid <harunmail19@gmail.com>
2023-05-16 21:27:38 +0300

    signin.tsx & singup.tsx modified

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,22 +1,37 @@
 import { useState } from 'react';
-import { IonInput, IonButton, IonItem } from '@ionic/react';
+import { IonInput, IonButton, IonItem, IonPage, IonCard } from '@ionic/react';
 import Home from './Home';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
+import SignIn from './signin';
+import './SignUp.css'
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-      <div>
-      <h1 className="d-flex justify-content-center">Sign up</h1>
-      <div className="d-flex justify-content-center">
-        <form onSubmit={handleSignUp} style={{ width: '90%', maxWidth: '25rem', backgroundColor: 'rgb(229, 224, 218)', padding: '20px', borderRadius: '20px', boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)' }}>
+     <IonCard className='info-container'>
+     
+        <form onSubmit={handleSignUp}>
+        <h1 style={{ color: 'rgb(255, 102, 0)', textAlign: 'center', marginBottom: '20px' }}>Sign up</h1>
           <div className="mb-3">
             <label>Email address</label>
-            <IonInput type="email" placeholder="Enter email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} style={{ borderRadius: '20px', backgroundColor: 'white' }} />
+            <IonInput className='input-item' type="email" placeholder="Enter email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} style={{ borderRadius: '20px', backgroundColor: 'white' }} />
           </div>
           <div className="mb-3">
             <label>Password</label>
-            <IonInput type="password" placeholder="Password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} style={{ borderRadius: '20px', backgroundColor: 'white' }} />
+            <IonInput className='input-item' type="password" placeholder="Password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} style={{ borderRadius: '20px', backgroundColor: 'white' }} />
           </div>
           <div className="d-flex justify-content-end">
             <IonButton type="submit" expand="block" >
@@ -24,15 +39,11 @@ function SignUp() {
             </IonButton>
           </div>
         </form>
-      </div>
-      <br />
-      <div className="d-flex justify-content-center">
-      <IonItem lines="full">
-           <IonItem ><h3 color="">Already signed up?</h3></IonItem>
-           <IonItem routerLink="/SignIn"><h5>Sign in!</h5></IonItem>
-      </IonItem>
-      </div>  
-      </div>
+      <IonItem lines="full" className="ionitem">
+           <IonItem className='ionitem'>Already signed up?</IonItem>
+           <IonButton routerLink="/signin">Sign in!</IonButton>
+      </IonItem> 
+      </IonCard>
   );
 };
 
